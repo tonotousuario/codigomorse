a288134059b1f7505644853b62c9b60a28135210
Kedap <kedap.dev@protonmail.com>
2024-12-14 01:11:47 -0600

    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # codigomorse
-...---...
+
+## Setup
+
+Primero se tiene que crear un entorno virtual para instalar las dependencias.
+
+```cmd
+python -m venv .venv
+pip install -r requirements.txt
+```
---
+++ src/morse.py
@@ -0,0 +1,11 @@
+def obtener_codificacion(texto: str) -> list[str]:
+    """
+    Este texto tiene que estar en formato morse (.,-, , ), espacio para separar
+    letras y barra para separar palabras esta funcion tiene que estos
+    caracteres a una lista con puntos y rayas y espacios para palabras.
+
+    Por ejemplo:
+        Entrada: ".--. --- -.. .-. .. .-   .... .- -.-. . .-.   . ... - ---   - --- -.. ---   . .-..   -.. .. .- .-.-."
+        Salida: [".--.","---","-..",".-.","..",".-"," ","....",".-","-.-.",".",".-."," ",".","...","-","---"," ","-","---","-..","---"," ",".",".-.."," ","-..","..",".-",".-.-."]
+    """
+    return []
