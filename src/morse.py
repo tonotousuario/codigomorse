352ae0dd70863660a342bc64a2e2a446039cf696
Megiddo <juancholo182005@gmail.com>
2024-12-16 00:12:54 -0600

    Pasando test

--- src/morse.py
+++ src/morse.py
@@ -1,11 +1,6 @@
 def obtener_codificacion(texto: str) -> list[str]:
-    """
-    Este texto tiene que estar en formato morse (.,-, , ), espacio para separar
-    letras y barra para separar palabras esta funcion tiene que estos
-    caracteres a una lista con puntos y rayas y espacios para palabras.
-
-    Por ejemplo:
-        Entrada: ".--. --- -.. .-. .. .-   .... .- -.-. . .-.   . ... - ---   - --- -.. ---   . .-..   -.. .. .- .-.-."
-        Salida: [".--.","---","-..",".-.","..",".-"," ","....",".-","-.-.",".",".-."," ",".","...","-","---"," ","-","---","-..","---"," ",".",".-.."," ","-..","..",".-",".-.-."]
-    """
-    return []
+    texto = texto.split(" ")
+    for i in range(len(texto)):
+        if texto[i] == "/":
+            texto[i] = " "
+    return texto
